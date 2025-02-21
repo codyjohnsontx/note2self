e5028a114898c632953dae55f6dab4aacd843ae9
Cody Johnson <codyjohnsontx@gmail.com>
2025-02-21 12:41:40 -0600

    modify App.tsx Create NoteList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<NoteList availableTags={tags} />} />
+        <Route path="/" element={<NoteList availableTags={tags} notes={notesWithTags} />} />
         <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
